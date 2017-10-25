5f5316638b2d0e990f21847f39b16686fe0008bf
roberto18 <robertochr92@gmail.com>
2017-10-25 13:48:01 +0200

    hjgg

--- cifocar/model/UsuarioModel.php
+++ cifocar/model/UsuarioModel.php
@@ -1,6 +1,6 @@
 <?php
 	class UsuarioModel{
-		//PROPIEDADES estamos provando 
+		//PROPIEDADES estamos provando jhdIHUI
 		public $id, $user, $password, $nombre, $privilegio=100, $admin=0, $email, $imagen='', $fecha;
 			
 		//METODOS
